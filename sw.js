2d601686ca1a35345c2794208b13fb5d86a0e6b4
antonionetopxxp <159489586+antonionetopxxp@users.noreply.github.com>
2026-09-17 02:40:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,145 +1,91 @@
-const CACHE = 'ntp-radio-v4';
+const CACHE = "ntp-radio-v2";
 
 const PRECACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './galaxy-theme.css',
-  './script.js',
-  './manifest.webmanifest',
-  './favicon.svg'
+  "./",
+  "./index.html",
+  "./style.css",
+  "./galaxy-theme.css",
+  "./script.js",
+  "./favicon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./whatsapp.svg",
+  "./instagram.svg"
 ];
 
-self.addEventListener('install', event => {
-
+self.addEventListener("install", (event) => {
   event.waitUntil(
-
     caches
       .open(CACHE)
-
-      .then(cache =>
-        cache.addAll(PRECACHE)
-      )
-
-      .then(() =>
-        self.skipWaiting()
-      )
-
+      .then((cache) => cache.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
   );
-
 });
 
-
-self.addEventListener('activate', event => {
-
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-
-    caches.keys()
-
-      .then(keys =>
-
-        Promise.all(
-
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
           keys
-
-            .filter(key =>
-              key !== CACHE
-            )
-
-            .map(key =>
-              caches.delete(key)
-            )
-
-        )
-
-      )
-
-      .then(() =>
-        self.clients.claim()
-      )
-
+            .filter((key) => key !== CACHE)
+            .map((key) => caches.delete(key))
+        );
+      })
+      .then(() => self.clients.claim())
   );
-
 });
 
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
 
-self.addEventListener('fetch', event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
 
+  /*
+   * Nunca colocar o stream da rádio no cache.
+   */
   if (
-    event.request.method !== 'GET'
+    url.hostname === "stream.zeno.fm" ||
+    url.hostname === "api.zeno.fm" ||
+    url.hostname.endsWith(".surfernetwork.com")
   ) {
     return;
   }
 
-  const url =
-    new URL(event.request.url);
-
-
   /*
-    NÃO colocar o streaming no cache.
-  */
-
-  if (
-    url.hostname === 'stream.zeno.fm' ||
-    url.hostname === 'api.zeno.fm'
-  ) {
-    return;
-  }
-
-
-  /*
-    Arquivos do próprio site
-  */
-
-  if (
-    url.origin === location.origin
-  ) {
-
+   * Arquivos do próprio site:
+   * primeiro tenta rede;
+   * se estiver offline, usa cache.
+   */
+  if (url.origin === location.origin) {
     event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
 
-      caches.match(
-        event.request
-      )
-
-      .then(cached => {
-
-        if (cached) {
-          return cached;
-        }
-
-        return fetch(
-          event.request
-        )
-
-        .then(response => {
-
-          if (
-            response &&
-            response.ok
-          ) {
-
-            const copy =
-              response.clone();
-
-            caches
-              .open(CACHE)
-              .then(cache =>
-                cache.put(
-                  event.request,
-                  copy
-                )
-              );
-
-          }
+          caches
+            .open(CACHE)
+            .then((cache) => {
+              cache.put(event.request, copy);
+            });
 
           return response;
-
-        });
-
-      })
-
+        })
+        .catch(() => {
+          return caches.match(event.request);
+        })
     );
 
+    return;
   }
 
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
 });
