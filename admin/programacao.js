366ffe7c736bbcebb21b6512258cbc3e4607b927
antonionetopxxp <159489586+antonionetopxxp@users.noreply.github.com>
2026-09-19 03:10:48 +0900

    Update programacao.js

--- admin/programacao.js
+++ admin/programacao.js
@@ -1,1 +1,14 @@
+<script>
+  document
+    .getElementById("logoutButton")
+    .addEventListener("click", function () {
 
+      sessionStorage.removeItem(
+        "ntp_admin_logged"
+      );
+
+      window.location.href =
+        "index.html";
+
+    });
+</script>
